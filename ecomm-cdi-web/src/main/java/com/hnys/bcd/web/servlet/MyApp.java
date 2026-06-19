b831014039aa13a879648200e685e8b283a5d247
Yohan3128 <Yohansilva560@gmail.com>
2026-06-19 12:38:25 +0530

    Add MyApp and MyService classes with basic structure

---
+++ ecomm-cdi-web/src/main/java/com/hnys/bcd/web/servlet/MyApp.java
@@ -0,0 +1,4 @@
+package com.hnys.bcd.web.servlet;
+
+public class MyApp {
+}
---
+++ ecomm-cdi/src/main/java/com/hnys/bcd/cdi/MyService.java
@@ -0,0 +1,7 @@
+package com.hnys.bcd.cdi;
+
+public class MyService {
+    public  void doSomething(){
+        System.out.println("MyService doSomething...");
+    }
+}
